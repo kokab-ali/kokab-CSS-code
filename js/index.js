62d4e079e27218e19395bab8e8ab98f954386a60
codecraftlearning <codecraftlearning@gmail.com>
2025-09-16 20:32:20 +0530

    Js 1st class

---
+++ js/index.js
@@ -0,0 +1,31 @@
+
+// var fruitName = 'Apple';
+// fruitName = 'Banana';
+// fruitName = 'Mango';
+
+// console.log(fruitName);
+
+// var fruitName = 'Apple';
+// var fruitName = 'Mango';
+// fruitName = 'Banana';
+// fruitName = 100;
+// fruitName = 'tree';
+// console.log(fruitName);
+
+
+// let carName = 'Maruti';
+// let carName = 'taxi';
+// carName = 'verna';
+// console.log(carName);
+
+
+// const myName = 'Ankit';
+// myName = 'Kokab';
+// console.log(myName);
+
+
+let myName;    //   <------- Variable declaration
+myName = 'Ankit';   //    <------- variable initialization
+let buildingNumber = 105;  // <------ variable defination
+
+console.log(myName);
