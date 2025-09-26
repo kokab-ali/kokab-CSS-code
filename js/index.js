181661986e6e6d1f808a74d509a6d1952ae7d7e2
codecraftlearning <codecraftlearning@gmail.com>
2025-09-26 20:00:01 +0530

    variables and operators

--- js/index.js
+++ js/index.js
@@ -1,31 +1,67 @@
 
-// var fruitName = 'Apple';
-// fruitName = 'Banana';
-// fruitName = 'Mango';
-
-// console.log(fruitName);
-
-// var fruitName = 'Apple';
-// var fruitName = 'Mango';
-// fruitName = 'Banana';
-// fruitName = 100;
-// fruitName = 'tree';
-// console.log(fruitName);
 
 
-// let carName = 'Maruti';
-// let carName = 'taxi';
-// carName = 'verna';
-// console.log(carName);
+let num1 = 10;
+let num2 = 10;
+let num3 = 15;
+
+let sum = num1 + num2 + num3;
+let multiplication = num1 * num2;
+let sub = num1 - num2;
+let div = num1 / num2;
+let isEqual = num1 === num2;
+
+console.log(sum);
+console.log(multiplication);
+console.log(sub);
+console.log(div);
+console.log(isEqual);
 
 
-// const myName = 'Ankit';
-// myName = 'Kokab';
-// console.log(myName);
+let name1 = "ankit";
+let name2 = "ankit";
+let isNameSame = name1 === name2;
+console.log(isNameSame);
 
 
-let myName;    //   <------- Variable declaration
-myName = 'Ankit';   //    <------- variable initialization
-let buildingNumber = 105;  // <------ variable defination
+let value1 = 100;
+let value2 = "100";
+let isValueEqual = value1 === value2;
+console.log(isValueEqual);
 
-console.log(myName);
+let isGreater = num1 > num2;
+console.log(isGreater);
+
+let age1 = 40;
+let age2 = 30;
+let ageSum = age1 + age2;
+
+let result1 = age1 === age2 || ageSum > 50;
+let result2 = age1 === age2 && ageSum > 50;
+
+console.log("age", result1);
+console.log("age", result2);
+
+
+/*
+    true && true = true
+    true && false = false
+    true || true = true
+    true || false = true
+    false || false = false
+*/
+/*
+    = assignment operator
+    + sum operator
+    - sunstraction operator
+    * multiplication operator
+    / division operator
+    === equal operator  (boolean value => true / false) (with type matching)
+    == equal operator (without type matching)
+    > greater than operator
+    < less than operator
+    >= greater than equal to operator
+    <= less than equal to operator
+    || or operator 
+    && and operator 
+*/
