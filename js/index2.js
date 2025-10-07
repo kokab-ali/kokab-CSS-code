b65d83640c4b2fa17df5c035586f02057eed375e
kokab-ali <kokabali165@gmail.com>
2025-10-07 18:47:22 +0530

    js

---
+++ js/index2.js
@@ -0,0 +1,54 @@
+
+
+let num1 = 10;
+let num2 = 20;
+let num3 = 30;
+let num4 = 40;
+let num5 = 80;
+
+let sum = num1 + num2 + num3 + num4 + num5;
+let multiplication = num1 * num5;
+let sub = num1 - num2;
+let div = num1 / num2;
+let isEqual = num1 === num5;
+
+console.log(sum)
+console.log(multiplication)
+console.log(sub)
+console.log(div)
+console.log(isEqual);
+
+
+let name1 = "Kokab";
+let name2 = 'F';
+let name3 = "Ali";
+let isNameSame = name1 === name2 === name3;
+console.log(isNameSame);
+
+
+let value1 = 200;
+let value2 = 201;
+let isValueEqual = value1 === value2;
+console.log(isValueEqual);
+
+let isGreater = num1 > num5;
+console.log(isGreater);
+
+
+let age1 = 40;
+let age2 = 50;
+let age3 = 30;
+let age4 = 20;
+let age5 = 55;
+let agesum = age1 + age2 + age3 + age4 + age5;
+
+
+let result1 = age1 === age2 || agesum > 80
+let result2 = age3 === age4 || agesum > 55
+let result3 = age3 === age5 && agesum > 80
+
+console.log("age", result1);
+console.log("age", result2);
+console.log("age", result3);
+
+
---
+++ js/index3.js
@@ -0,0 +1,15 @@
+let num1 = 10;
+let num2 = 20;
+let num3 = 50;
+let sum = num1 + num2;
+console.log(sum);
+
+let sub = sum - num3;
+console.log(sub);
+
+let multiplication = sum * sub;
+console.log(multiplication)
+
+// write a program which takes 3 number and store it in variables then check if all 3 variables are equal or not
+
+// write a program where you take 1 variable and store any number then add it 10 times consicutably 
