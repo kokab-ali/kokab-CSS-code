3aa2ee464b9b480655a59ed527ef3dc657048402
kokab-ali <kokabali165@gmail.com>
2025-10-08 16:18:02 +0530

    program js

---
+++ js/program.js
@@ -0,0 +1,17 @@
+
+
+let num = 10;
+let sum = 0;
+
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+sum = sum + num;
+
+console.log(sum)
