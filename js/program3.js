68b9b0e0ee7d1a4c465c0b9faf874fbdad34e1e9
kokab-ali <kokabali165@gmail.com>
2025-10-13 18:07:09 +0530

    js program

---
+++ js/program3.js
@@ -0,0 +1,20 @@
+
+
+let num1 = 10;
+let num2 = 20;
+
+let isEqual = num1 === num2;
+
+console.log(isEqual)
+
+let name1 = "Kokab";
+let name2 = "Ali";
+let name3 = "F";
+let name4 = "Kokab";
+let isNameSame = name1 === name2 === name3 === name4;
+let isValueEqual = name1 === name2 === name3 === name4;
+let isNameSame2 = name1 === name4;
+
+console.log(isNameSame)
+console.log(isValueEqual)
+console.log(isNameSame2)
