2daf499fb7cd19a09a78dab273d585914c5ff6bd
kokab-ali <kokabali165@gmail.com>
2025-11-17 18:47:26 +0530

    java script conditional statement

--- js/conditional.statement.js
+++ js/conditional.statement.js
@@ -28,3 +28,33 @@ else(console.log('normalcitizen')
 
 // agar 60 se kam h to kitne saal lgege 60 hone me.('- 60' = numberofyears) 
 // agar 60 se jyada h to kitne saal 60 se jyada h.('+ 60' = numberofyears)
+
+let age2 = 75;
+let numberOfYears = 60 + age2;
+
+console.log(numberOfYears);
+
+/*let age3 = 72;
+if (age3 = 60) {
+    console.log('ageis',age3);
+    
+    
+}
+else if (age3 > 60) {
+    console.log('age3 > 60', age3 - 60);
+    
+}
+else(console.log('age < 60', age3 - 60)
+)*/
+
+let age3 = 60;
+
+if (age3 < 60 && age3 === 60) {
+    console.log('age3 < 60', 60 - age3);
+    
+}
+/*else if (age3 === 60) {
+    console.log('age3 = 60');
+    
+}*/
+else console.log('age3 > 60', age3 - 60);
