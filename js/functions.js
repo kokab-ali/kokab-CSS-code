0c5923ea7dbc9ee35abf5b187a6c3023ee642727
kokab-ali <kokabali165@gmail.com>
2025-11-19 19:28:06 +0530

    java script

---
+++ js/functions.js
@@ -0,0 +1,57 @@
+
+let a = 3;
+let b = 2;
+
+sum = a + b;
+console.log(sum);
+let c = 4;
+let d = 5;
+
+add = c + d;
+console.log(add);
+
+function addition(parameter1, parameter2) {
+  let addedsum = parameter1 + parameter2;
+    return addedsum; 
+}
+
+addition (a, b)
+
+
+function totalfruits(fruit1, fruit2, fruit3) {
+    let totalfruit = fruit1 + fruit2 + fruit3;
+    return console.log(totalfruit);
+    
+}
+
+let mango = 10;
+let apple = 20;
+let banana = 30;
+
+totalfruits(apple, banana, mango)
+
+function multiply(num1, num2) {
+    let num01 = num1 * num2;
+    return answer;
+}
+
+// 2 number hai unka squar add hoke ans, 
+
+let temp = 20;
+let isRaining = false;
+let hasUmbrella = true;
+
+let result = (temp > 20 && isRaining === false) || (hasUmbrella && !isRaining)
+console.log(result);
+
+
+// false && true = true
+
+// temp > 20 (false) && israining (true) = (false)
+
+// hasumb (true) && israining (true) 
+
+
+
+
+
